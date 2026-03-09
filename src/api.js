932430fe0e49c7b1b73a87f1ff0d41093ad43c72
unknown <ricvera@favoritafc.com>
2026-03-09 01:17:10 -0500

    Utilizar Backend URL interna que no es sobre HTTPS

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
+const BASE_URL = '/api';  // se usará el proxy de Vite
+
 export const fetchOrders = async () => {
-  const response = await fetch('http://backend-app:5000/orders');
+  const response = await fetch('${BASE_URL}/orders');
   if (!response.ok) throw new Error('Failed to fetch orders');
   return response.json();
 };
