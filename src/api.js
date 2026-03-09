72947859003d42b8254f426be9469d5f1be9c95a
unknown <ricvera@favoritafc.com>
2026-03-09 00:56:25 -0500

    Utilizar la FQDN pero sin el ID de Terraform

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export const fetchOrders = async () => {
-  const response = await fetch('http://backend-app--8h3ry7m.internal.livelydesert-6725c03a.westus2.azurecontainerapps.io:5000/orders');
+  const response = await fetch('http://backend-app.internal.livelydesert-6725c03a.westus2.azurecontainerapps.io:5000/orders');
   if (!response.ok) throw new Error('Failed to fetch orders');
   return response.json();
 };
