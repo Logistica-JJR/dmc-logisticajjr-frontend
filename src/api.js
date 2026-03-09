a3eebb9c3756989222a4f94072170c6d724b22d3
unknown <ricvera@favoritafc.com>
2026-03-09 01:03:00 -0500

    Utilizar HTTPS de Backend

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export const fetchOrders = async () => {
-  const response = await fetch('http://backend-app.internal.livelydesert-6725c03a.westus2.azurecontainerapps.io:5000/orders');
+  const response = await fetch('https://backend-app.internal.livelydesert-6725c03a.westus2.azurecontainerapps.io:5000/orders');
   if (!response.ok) throw new Error('Failed to fetch orders');
   return response.json();
 };
